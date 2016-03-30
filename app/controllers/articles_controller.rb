01fdfc7a66bcd7195b6fe70d01bc1000c9794361
Shloime Weinberger <shloimew99@gmail.com>
2016-03-30 19:15:34 +0000

    associations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def update
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -35,7 +36,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    
+
   end
 
   def destroy
@@ -45,11 +46,11 @@ class ArticlesController < ApplicationController
   end
 
   private
-  
+
   def set_article
     @article = Article.find(params[:id])
   end
-  
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
