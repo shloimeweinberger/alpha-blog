e0cf4edf4f228475a5a983e8d36485870048871d
Shloime Weinberger <shloimew99@gmail.com>
2016-06-01 22:04:12 +0000

    belongs_to :user, dependent: :destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ActiveRecord::Base
 
-  validates :name, presence: true,
-                   uniqueness: true ,
-                   length: {minimum: 3, maximum: 25}
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
+  validates :name, presence: true, uniqueness: true , length: {minimum: 3, maximum: 25}
 
 end
